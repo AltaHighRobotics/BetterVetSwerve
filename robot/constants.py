54d34de60c1720e4f31886b261f8c880d65b69fb
Ethanscharlie <ethanscharlie@protonmail.com>
2025-02-20 16:25:24 -0700

    Cleaned up constants file, not fixed in rest of codebase

--- robot/constants.py
+++ robot/constants.py
@@ -1,79 +1,69 @@
-import math
 
-# Input
-kDriverControllerPort = 0
-kDeadband = .2
-kTurnDeadband = .5
+from math import tau
 
-# States
-kDefaultObjective = "I"
-kDefaultBucket = 0
-kDefaultIntake = 0
-kDefaultEndStop = -1
-kDefaultEndStopOverride = 0
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Input
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+DRIVER_CONTROLLER_PORT = 0
+DEADBAND = 0.2
+TURN_DEADBAND = 0.5
 
-# Swerve
-kModuleMaxAngularVelocity = math.tau # Keep as it
-kModuleMaxAngularAcceleration = 2*math.tau # Keep as is
-kSwerveTurnGearRatio = 1/180#1 / 250 # (1/160) how many times the wheel rotates for every revolution of the turn motor
-# Dimensions for kinematics
-kSwerveModCtrToCtr = .635 # How far apart the modules are, not diagnol
-kWheelRadius = 0.0508 # Meter
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Default States
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+DEFAULT_OBJECTIVE = "I"
+DEFAULT_BUCKET = 0
+DEFAULT_INTAKE = 0
+DEFAULT_END_STOP = -1
+DEFAULT_END_STOP_OVERRIDE = 0
 
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Swerve math
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+MODULE_MAX_ANGULAR_VELOCITY = tau
+MODULE_MAX_ANGULAR_ACCELERATION = 2 * tau
+SWERVE_TURN_GEAR_RATIO = 1/180 # Rotations from motor to wheel
+SWERVE_MOD_CENTER_TO_CENTER = 0.635 # How far apart the modules are, not diagnol
+WHEEL_RADIUS = 0.0508 # In Meters
 
-# Enstops
-kOutEndstopPort = 3
-kInEndstopPort = 2
-kEndstopInversion = True
-# Speeds
-kBucketSpeed = .9
-kIntakeSpeed = .8
-kOuttakeSpeed = .4
-kSwerveMinSpeed = .3
-kSwerveMaxSpeed = .8 # 1
-kSwerveMaxOutput = .6 # .8
-# MotorIDs
-# Bucket
-kBucketID = 9
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Endstops
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+OUT_ENDSTOP_PORT = 3
+IN_ENDSTOP_PORT = 2
+ENDSTOP_INVERSION = True
 
-#Intake
-kIntakeLID = 10
-kIntakeRID = 11
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Speeds
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+BUCKET_SPEED = 0.9
+INTAKE_SPEED = 0.8
+OUTTAKE_SPEED = 0.4
+SWERVE_MIN_SPEED = 0.3
+SWERVE_MAX_SPEED = 0.8
+SWERVE_MAX_OUTPUT = 0.6
 
-# Front Left
-kFLDriveID = 3
-kFLTurnID = 33
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+    Motor IDS
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+BUCKET_ID = 9
 
-# Front Right
-kFRDriveID = 4
-kFRTurnID = 44
+INTAKE_LEFT_ID = 10
+INTAKE_RIGHT_ID = 11
 
-# Back Left
-kBLDriveID = 2
-kBLTurnID = 22
+FRONT_LEFT_DRIVE_ID = 3
+FRONT_LEFT_TURN_ID = 33
 
-# Back Right
-kBRDriveID = 5
-kBRTurnID = 55
+FRONT_RIGHT_DRIVE_ID = 4
+FRONT_RIGHT_TURN_ID = 44
 
-# Turn PID
-# Front Left
-kTurnFLP = 10#13
-kTurnFLI = 0#0
-kTurnFLD = .3#.3
+BACK_LEFT_DRIVE_ID = 2
+BACK_LEFT_TURN_ID = 22
 
-# Front Right
-kTurnFRP = kTurnFLP + 0
-kTurnFRI = kTurnFLI + 0
-kTurnFRD = kTurnFLD + 0
+BACK_RIGHT_DRIVE_ID = 5
+BACK_RIGHT_TURN_ID = 55
 
-# Back Left
-kTurnBLP = kTurnFLP + 0
-kTurnBLI = kTurnFLI + 0
-kTurnBLD = kTurnFLD + 0
-
-# Back Right
-kTurnBRP = kTurnFLP + 0
-kTurnBRI = kTurnFLI + 0
-kTurnBRD = kTurnFLD + 0
+TURN_P = 10
+TURN_I = 0
+TURN_D = 0.3
 
