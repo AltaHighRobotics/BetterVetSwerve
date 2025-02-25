5644d2a745146c1f305860015483f6e880d7fcef
Duncan <143680478+Tiny0pus@users.noreply.github.com>
2025-02-24 17:41:32 -0700

    NOT WORKING Added Apriltag subsystem, modified constants, testing out online code, added utils

--- robot/subsystems/aprilTagSubsystem.py
+++ robot/subsystems/aprilTagSubsystem.py
@@ -1,9 +1,12 @@
 from photonlibpy.photonCamera import PhotonCamera
 from commands2 import Subsystem
-from photonlibpy.photonTrackedTarget import PhotonTrackedTarget
+# from photonlibpy.photonTrackedTarget import PhotonTrackedTarget
 from utils.flag import Flag
 
 from constants import KCAM_NAME
+
+from constants import SWERVE_MAX_SPEED
+import constants
 
 class AprilTagSubsystem(Subsystem): # Apriltags with PhotonVision
     def __init__(self) -> None:
@@ -14,19 +17,19 @@ class AprilTagSubsystem(Subsystem): # Apriltags with PhotonVision
         self.flag = None
         self.targets = []
 
-    def getHighestID(self, targets: list[PhotonTrackedTarget]) -> PhotonTrackedTarget: # get the target with the highest fiducial ID
-        bestTarget = targets[0]
-        for target in targets:
-            if target.getFiducialId() > bestTarget.getFiducialId():
-                bestTarget = target
-        return bestTarget
+    # def getHighestID(self, targets: list[PhotonTrackedTarget]) -> PhotonTrackedTarget: # get the target with the highest fiducial ID
+    #     bestTarget = targets[0]
+    #     for target in targets:
+    #         if target.getFiducialId() > bestTarget.getFiducialId():
+    #             bestTarget = target
+    #     return bestTarget
 
-    def refresh(self) -> list[PhotonTrackedTarget]: # Get latest target data
-        result = self.cam.getLatestResult()
-        if result.hasTargets():
-            self.targets = result.getTargets()
-            return self.targets
-        else: return None
+    # def refresh(self) -> list[PhotonTrackedTarget]: # Get latest target data
+    #     result = self.cam.getLatestResult()
+    #     if result.hasTargets():
+    #         self.targets = result.getTargets()
+    #         return self.targets
+    #     else: return None
     
     def hasTarget(self, id: int = None) -> bool: # See if a target with a given fiducial is visible. Run with no args to see if any targets are visible
         targets = self.targets
@@ -52,3 +55,35 @@ class AprilTagSubsystem(Subsystem): # Apriltags with PhotonVision
 
     def isFlagged(self):
         return self.flagOn
+
+    #### MAKE THIS WORK #### source: https://docs.photonvision.org/en/v2025.2.1-rc2/docs/examples/aimingatatarget.html
+    def teleopPeriodic(self) -> None:
+
+        xSpeed = -1.0 * self.controller.getLeftY() * constants.KMAX_SPEED
+        ySpeed = -1.0 * self.controller.getLeftX() * constants.KMAX_SPEED
+        rot = -1.0 * self.controller.getRightX() * constants.KMAX_ANGULAR_SPEED
+
+
+        # Get information from the camera
+        targetYaw = 0.0
+        targetVisible = False
+        results = self.cam.getAllUnreadResults()
+        if len(results) > 0:
+            result = results[-1]  # take the most recent result the camera had
+            for target in result.getTargets():
+                if target.getFiducialId() == 7:
+                    # Found tag 7, record its information
+                    targetVisible = True
+                    targetYaw = target.getYaw()
+
+
+        if self.controller.getAButton() and targetVisible:
+
+            # Driver wants auto-alignment to tag 7
+            # And, tag 7 is in sight, so we can turn toward it.
+            # Override the driver's turn command with an automatic one that turns toward the tag.
+
+            rot = -1.0 * targetYaw * constants.KAUTO_ALIGN_P * constants.KMAX_ANGULAR_SPEED
+
+
+        self.swerve.drive(xSpeed, ySpeed, rot, True, self.getPeriod())
